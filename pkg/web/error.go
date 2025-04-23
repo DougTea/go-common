e4030ad54ff9d966de18ad476d050e9a1a581ad6
jiaqing.kuang <jiaqing.kuang@westwell-lab.com>
2025-04-23 10:34:48 +0800

    add Unwarp and msg format

--- pkg/web/error.go
+++ pkg/web/error.go
@@ -1,6 +1,11 @@
 package web
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+)
 
 type ErrorCode string
 
@@ -17,10 +22,10 @@ type Error struct {
 	Time  time.Time
 }
 
-func NewError(code ErrorCode, msg string) *Error {
+func NewError(code ErrorCode, msg string, args ...interface{}) *Error {
 	return &Error{
 		Code:  code,
-		Msg:   msg,
+		Msg:   fmt.Sprintf(msg, args...),
 		Cause: nil,
 		Time:  time.Now(),
 	}
@@ -35,18 +40,32 @@ func NewErrorWithCause(code ErrorCode, cause error) *Error {
 	}
 }
 
-func NewErrorWithCauseAndMsg(code ErrorCode, cause error, msg string) *Error {
+func NewErrorWithCauseAndMsg(code ErrorCode, cause error, msg string, args ...interface{}) *Error {
 	return &Error{
 		Code:  code,
-		Msg:   msg,
+		Msg:   fmt.Sprintf(msg, args...),
 		Cause: cause,
 		Time:  time.Now(),
 	}
 }
 
-func (a *Error) Error() string {
-	if a.Msg != "" {
-		return a.Msg
+func (e *Error) Error() string {
+	if e.Msg != "" {
+		return e.Msg
 	}
-	return a.Cause.Error()
+	return e.Cause.Error()
+}
+
+func (e *Error) Unwrap() error {
+	return e.Cause
+}
+
+func (e *Error) ErrorWithCause() string {
+	msg := new(strings.Builder)
+	msg.WriteString(e.Msg)
+	for err := e.Cause; err != nil; err = errors.Unwrap(err) {
+		msg.WriteString("caused by: ")
+		msg.WriteString(err.Error())
+	}
+	return msg.String()
 }
